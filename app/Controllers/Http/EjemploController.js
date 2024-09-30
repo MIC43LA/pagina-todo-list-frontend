d2208bb7c2dba98cfc56f090ed28be3c6fedd516
Micaela <micaflaherty18@gmail.com>
2024-09-30 18:18:31 -0300

    agregado layout y parciales

--- app/Controllers/Http/EjemploController.js
+++ app/Controllers/Http/EjemploController.js
@@ -4,28 +4,52 @@ const fs = require('fs')
 const path = require('path')
 const Handlebars = require('handlebars')
 
+// Registrar los partials
+const partialsDir = path.join(__dirname, '../../../resources/views/partials')
+fs.readdirSync(partialsDir).forEach(filename => {
+  const partialName = path.parse(filename).name
+  const partialPath = path.join(partialsDir, filename)
+  const partialTemplate = fs.readFileSync(partialPath, 'utf-8')
+  Handlebars.registerPartial(partialName, partialTemplate) // Registrar partial con Handlebars
+})
+
 class EjemploController {
-  
   /*----------------------------------------------------------------------------------------------------------*/
   /*-----------------------------------------------METODO 1---------------------------------------------------*/
   /*----------------------------------------------------------------------------------------------------------*/
-    // metodfo 1 para manejar la solicitud GET y renderizar vista1.hbs
+  // Método 1 para manejar la solicitud GET y renderizar vista1.hbs
   async index({ response }) {
-    const rutaArchivo = path.join(__dirname, '../../../resources/views/index.hbs') // Ruta a la plantilla Handlebars
-    const plantillaCargada = fs.readFileSync(rutaArchivo, 'utf-8') // Leer el archivo de la plantilla
-    const plantillaCompilada = Handlebars.compile(plantillaCargada) // Compilar la plantilla
-    
-    // Datos que se enviaran a la plantilla
+    // Ruta al layout base
+    const layoutPath = path.join(__dirname, '../../../resources/views/layouts/main.hbs')
+    const layoutContent = fs.readFileSync(layoutPath, 'utf-8')
+    const layoutTemplate = Handlebars.compile(layoutContent)
+
+    // Ruta a la vista (contenido principal)
+    const viewPath = path.join(__dirname, '../../../resources/views/index.hbs')
+    const viewContent = fs.readFileSync(viewPath, 'utf-8')
+    const viewTemplate = Handlebars.compile(viewContent)
+
+    // Datos que se enviarán a la vista
     const datos = {
       titulo: 'Vista 1 - Handlebars',
       mensaje: 'Esta es la primera vista renderizada con Handlebars',
-      datosAdicionales: 'Datos extra para la vista 1'
+      datosAdicionales: 'Datos extra para la vista 1',
     }
 
-    const html = plantillaCompilada(datos) // Renderizr la plantilla con los datos
-    response.send(html) // Enviar la respuesta con la plantila renderizada
+    // Renderizar la vista principal con los datos
+    const viewHtml = viewTemplate(datos)
+
+    // Datos para el layout (renderizar el contenido principal dentro del layout)
+    const layoutData = {
+      body: viewHtml, // Inserta la vista dentro del layout
+      titulo: 'Mi página con layout',
+    }
+
+    // Renderizar el layout completo con la vista dentro
+    const html = layoutTemplate(layoutData)
+
+    response.send(html) // Enviar la respuesta con el HTML final renderizado
   }
-  
   /*----------------------------------------------------------------------------------------------------------*/
   /*-----------------------------------------------METODO 2---------------------------------------------------*/
   /*----------------------------------------------------------------------------------------------------------*/

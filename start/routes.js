cd7d8a716d3100ce0e8244c95c9e8f69c0eabc84
roxymarchetto <roxanamarchetto@gmail.com>
2024-10-07 22:08:35 -0300

    archivo routes actualizado

--- start/routes.js
+++ start/routes.js
@@ -10,6 +10,14 @@ Route.get('/', 'EjemploController.index')
 Route.get('/dashboard', 'EjemploController.dashboard')
 Route.get('/vista3', 'EjemploController.metodo3')
 
+Route.get('/tasks', 'TaskController.index');        // Index
+Route.get('/tasks/create', 'TaskController.create'); // Create
+Route.get('/tasks/:id', 'TaskController.show');      // Show
+Route.post('/tasks', 'TaskController.store');        // Store
+Route.put('/tasks/:id', 'TaskController.update');    // Update
+Route.delete('/tasks/:id', 'TaskController.destroy');// Destroy
+
+
 //localhost:3333/       - Carga la el controlador de ejemplo con el metodo 1
 //localhost:3333/vista2 - Carga la el controlador de ejemplo con el metodo 2
 //localhost:3333/vista3 - Carga la el controlador de ejemplo con el metodo 3
